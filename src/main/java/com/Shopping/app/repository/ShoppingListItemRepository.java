db0bd922af9886ac41e6bef2dae0995b99722421
LychMax <120021835+LychMax@users.noreply.github.com>
2024-12-06 02:29:31 +0300

    Delete src/main directory

--- src/main/java/com/Shopping/app/repository/ShoppingListItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Shopping.app.repository;
-
-import com.Shopping.app.entity.ShoppingListItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingListItemRepository extends JpaRepository<ShoppingListItem, Long> {
-}
--- src/main/java/com/Shopping/app/repository/ShoppingListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Shopping.app.repository;
-
-import com.Shopping.app.entity.ShoppingList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
-}
