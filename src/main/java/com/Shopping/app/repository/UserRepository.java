5a47ce90e6d573229dff43a2e14b48677d4764b7
LychMax <120021835+LychMax@users.noreply.github.com>
2024-12-14 23:17:03 +0300

    Delete src/main directory

--- src/main/java/com/Shopping/app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Shopping.app.repository;
-
-import com.Shopping.app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
